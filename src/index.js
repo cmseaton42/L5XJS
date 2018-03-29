47483711d66f2c41f37f03516588a1323c1322bf
Canaan Seaton <cmseaton42@gmail.com>
2018-03-29 17:23:10 -0400

    Add new classes to global exports

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 const Document = require("./document");
 const Tag = require("./tag");
+const Program = require("./program");
+const Routine = require("./routine");
+const Rung = require("./rung");
+const Util = require("./utilities");
 
-module.exports = { Document, Tag };
+module.exports = { Document, Tag, Program, Routine, Rung, Util };
